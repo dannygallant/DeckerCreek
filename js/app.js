c55fd18a6056c6db553977b2ec420cde549afdd0
dannygallant <danny.gallant@att.net>
2015-09-11 17:47:47 -0500

    Minor changes to image & styling + app.js file start
    Added responsive classes & drop shadow to images. Added main site
    border. Created app.js file. Will probably store text in an array and
    populate main info area on button clicks. Images will probably launch
    galleries/youtube vids in modals.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	var info = [{
+		heading: "What you'll find at Decker Creek B&amp;B&amp;B:",
+		content: "Your own private cabin with screened porch,<br> fenced yard, kitchen, patio, grill, whirlpool tub<br> and many other amenities.<br>Dog bowls, towels, beds, crates (available if needed) and<br> homemade dog biscuits.<br>A full country breakfast from local farms.<br>Room to roam.<br>Area guides, resources and recommendations.<br>Nearby activities, including golf, canoeing and trail rides.<br>Downtown Austin just 14 miles away.",
+	},
+	{
+		heading: "Cabin 1: Hilltop",
+		content: "Cabin 1, 'Hilltop', is about 300 feet from our house and in view of the lightly used rural road. The whirlpool tub is in the living area with views of the countryside and the TV (with VCR and DVD players). The bath includes a spacious standing shower. The yard is mostly open with several shade trees.",
+		rates: "$169/night based on two nights or more. Two-night minimum on weekends.",
+		image:
+	},
+	{
+		heading: "Cabin 2: Treehouse",
+		content: "Cabin 2, 'Treehouse', is down the hill away from the road, about 200 feet from Cabin 1. Herer you are surrounded by trees, yet the porch is high off the ground. The whirlpool tub is in the bathroom, for more privacy. The yard is woodsy, with many trees and shrubs. First come, first served in choosing the cabin you want.",
+		rates: "$169/night based on two nights or more. Two-night minimum on weekends.",
+		image:
+	},
+	{
+		heading: "About Us",
+		content: "Decker Creek is owned and operated by Pay, Byron, Beau, Razr and Shiva Rathburn. We are members of the Texas Bed and Breakfast Association and the Professional Inkeepers Association.",
+		contact: "Email us: pat.rathburn@gmail.com"
+	}
+
+	]
+
+});  // Opening Document Ready
